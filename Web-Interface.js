3c9d5212d87383b87aa46b163138fd71614a6c4a
rfedericci <75981743+rfedericci@users.noreply.github.com>
2020-12-17 10:39:04 +0100

    Update Web-Interface.js

--- Web-Interface.js
+++ Web-Interface.js
@@ -2,6 +2,17 @@ var A;
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/42ee8532413f4c9b93840672260234bf"));
 
-web3.eth.getBlockNumber().then(async function(result){console.log(result);}); // ici result me retourne ne entier. Je veuix que cet entier soit ensuite injecté dans la variable A
-web3.eth.getBlock(9271021).then(async function(result){console.log(result);}); // 92711021 serait ici la Variable A, resilt retournera un autre entier que j'aimerais utiliser dans la suite du soft
-console.log(A);
+async function LatestBlock() {
+    let BlockNum =  await web3.eth.getBlockNumber().then(async function(result){return result;});
+    return BlockNum;
+};
+LatestBlock();
+
+async function DatafromBlock() {
+    let BlockNum = await LatestBlock();
+    let DataBlock = await web3.eth.getBlock(BlockNum).then(async function(result){return result;});
+    console.log(DataBlock);
+    return DataBlock;
+};
+DatafromBlock();
+
