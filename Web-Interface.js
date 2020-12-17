0b433f6741dcb45ebcbf23bfcdfe2a3f8cf4fee3
rfedericci <75981743+rfedericci@users.noreply.github.com>
2020-12-17 09:46:38 +0100

    Update Web-Interface.js

--- Web-Interface.js
+++ Web-Interface.js
@@ -2,13 +2,6 @@ var A;
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/42ee8532413f4c9b93840672260234bf"));
 
-//web3.eth.getBlockNumber().then(async function(result){console.log(result);});
-//web3.eth.getBlock(9271021).then(async function(result){console.log(result);});
-  
-
-const PromiseLatestBlock = Promise.resolve(web3.eth.getBlockNumber().then(async function(result){return result;}));
-PromiseLatestBlock.then((value) => {console.log(value);});
-
-ValuePromiseLatestBlock = PromiseLatestBlock.then((value) => {return value;});
-
-web3.eth.getBlock(PromiseLatestBlock.then((value) => {return value;})).then(async function(result){console.log(result);});
+web3.eth.getBlockNumber().then(async function(result){console.log(result);}); // ici result me retourne ne entier. Je veuix que cet entier soit ensuite injecté dans la variable A
+web3.eth.getBlock(9271021).then(async function(result){console.log(result);}); // 92711021 serait ici la Variable A, resilt retournera un autre entier que j'aimerais utiliser dans la suite du soft
+console.log(A);
